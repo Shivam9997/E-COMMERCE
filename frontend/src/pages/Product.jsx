184fc5726ea832d9ed6e60b7f644bd6ab19d59be
Shivam9997 <shivamrathore2805@gmail.com>
2026-04-17 18:15:13 +0530

    Add RelatedProducts component to Product page and filter by subCategory

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -120,7 +121,9 @@ const Product = () => {
 
         {/*  ---------Related Products Section ---------- */}
 
-        
+        <RelatedProducts category = {productData.category} subCategory={productData.subCategory} />
+ 
+
     </div>
   ) : (
     <div className="opacity-0"></div>
