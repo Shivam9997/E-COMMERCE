43c753bb65bdf661f834dbc1320374ad168b179c
Developer <user@example.com>
2026-04-19 19:29:02 +0530

    Add MongoDB connection setup for database integration

---
+++ backend/config/mongodb.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    
+    mongoose.connection.on('connected', ()=>{
+        console.log("db Connected")
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/E-COMMERCE`)
+}
+export default connectDB;
