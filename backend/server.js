2f04db5457f20eec861a61c9055767e741d38bff
Developer <user@example.com>
2026-04-19 19:28:33 +0530

    Add database connection to server initialization

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'dotenv/config'
 import cors from 'cors';
+import connectDB from './config/mongodb.js'
 
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB()
 
 // MiddleWare
 app.use(express.json());
@@ -16,5 +18,5 @@ app.get('/', (req,res)=>{
     res.send("API is working fine");
 })
 
-app.listen(port,()=> console.log(`localhost:${port}`))
+app.listen(port,()=> console.log(` server is running for localhost:${port}`))
 
