a123004e79c2c83fc87d0a170a19f2e5b32b28a7
Shivam9997 <shivamrathore2805@gmail.com>
2026-04-19 17:23:32 +0530

    Add Title component to Contact page for improved layout

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const Contact = () => {
+ 
+
   return (
     <div>
-      
+          <div className='text-center text-2xl pt-10 border-t' >
+            <Title text1={'CONTACT'} text2={'US'} />
+
+          </div>
+          <div className=' my-10 flex flex-col gap'>
+
+          </div>
     </div>
   )
 }
