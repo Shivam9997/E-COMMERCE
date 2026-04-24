4431e9f8c0cbcfce668da7e192a139fe4156796c
Shivam9997 <shivamrathore2805@gmail.com>
2026-04-25 03:50:46 +0530

    Add new Files

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { allOrders, placeOrder, updateStatus, userOrders } from '../controllers/orderController.js'
+import  adminAuth  from '../middleware/adminAuth.js'
+import  authUser from '../middleware/auth.js'
+
+
+const orderRouter = express.Router()
+
+orderRouter.post('/list',adminAuth, allOrders)
+orderRouter.post('/status',adminAuth, updateStatus)
+
+// Payment Features
+orderRouter.post('/place',authUser, placeOrder)
+
+
+// User features
+orderRouter.post('/userorders',authUser, userOrders)
+
+
+export default orderRouter
